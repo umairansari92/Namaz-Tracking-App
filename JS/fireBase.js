f7a7af9ee651d190d8c7fcdfd00bca332c8f318b
umairansari92 <umair.ansari.92@gmail.com>
2025-07-22 04:48:53 +0500

    first commit

---
+++ JS/fireBase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMaS8rDo_dDgvc1opvMDS_F3orwQ6F5RE",
+  authDomain: "namaz-tracking-app-d9c52.firebaseapp.com",
+  projectId: "namaz-tracking-app-d9c52",
+  storageBucket: "namaz-tracking-app-d9c52.firebasestorage.app",
+  messagingSenderId: "891734085961",
+  appId: "1:891734085961:web:0bda9f8c1d277b45388a98"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
